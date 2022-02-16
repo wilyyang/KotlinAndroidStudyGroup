d642f3005c11c30250f54b2ec1ea4e3359611607
wily.yang <ehdrnr1178@gmail.com>
2022-02-16 19:11:05 +0900

    page 621 complete

--- Chap19/app/src/main/java/com/example/chap19/MainActivity.kt
+++ Chap19/app/src/main/java/com/example/chap19/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.chap19
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Criteria
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,18 +12,36 @@ import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.chap19.databinding.ActivityMainBinding
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnSuccessListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?)!!.getMapAsync(this)
         requestPermission()
     }
 
     private val PERMISSIONS_REQUEST_CODE = 100
-    private var REQUIRED_PERMISSIONS = arrayOf( Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+    private var REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_NETWORK_STATE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
     private fun requestPermission(){
 
         var permissionCheck = true
@@ -34,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         if(!permissionCheck){
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
         }else{
-            mapTest()
+            googleMapTest()
         }
     }
 
@@ -63,10 +83,88 @@ class MainActivity : AppCompatActivity() {
                 val accuracy = location.accuracy
                 val time = location.time
 
-                Log.d("wily", "Last Location : $latitude, $longitude, $accuracy, $time")
+                Log.d("wily5", "Last Location : $latitude, $longitude, $accuracy, $time")
             }?: run {
-                Log.d("wily", "Not.. data")
+                Log.d("wily5", "Not.. data")
+            }
+
+            val listener: LocationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    val accuracy = location.accuracy
+                    val time = location.time
+
+                    Log.d("wily5", "Location : $latitude, $longitude, $accuracy, $time")
+
+                }
+
+                override fun onProviderEnabled(provider: String) {
+                    super.onProviderEnabled(provider)
+                }
+
+                override fun onProviderDisabled(provider: String) {
+                    super.onProviderDisabled(provider)
+                }
+            }
+
+            val provider = manager.getBestProvider(Criteria().apply {
+                isSpeedRequired = true
+            }, true)
+            Log.d("wily5", "result : $provider")
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000L, 1f, listener)
+//            manager.removeUpdates(listener)
+        }
+    }
+
+    private fun mapApiTest(){
+        val providerClient: FusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+
+        val connectionCallback = object: GoogleApiClient.ConnectionCallbacks {
+            override fun onConnected(p0: Bundle?) {
+                if(ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                    providerClient.lastLocation.addOnSuccessListener(this@MainActivity
+                    ) { location ->
+                        val latitude = location?.latitude
+                        val longitude = location?.longitude
+                        val accuracy = location?.accuracy
+                        val time = location?.time
+
+                        Log.d("wily6", "Location : $latitude, $longitude, $accuracy, $time")
+                    }
+                }
+            }
+
+            override fun onConnectionSuspended(p0: Int) {
+                Log.d("wily6", "onConnectionSuspended")
             }
         }
+
+        val onConnectionFailedCallback = GoogleApiClient.OnConnectionFailedListener { Log.d("wily6", "onConnectionFailed") }
+
+        val apiClient: GoogleApiClient = GoogleApiClient.Builder(this)
+            .addApi(LocationServices.API)
+            .addConnectionCallbacks(connectionCallback)
+            .addOnConnectionFailedListener(onConnectionFailedCallback)
+            .build()
+        apiClient.connect()
+    }
+
+    private fun googleMapTest(){
+
+    }
+
+    var googleMap: GoogleMap? = null
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap = map
+
+        val latLng = LatLng(37.566610, 126.978403)
+        val position = CameraPosition.Builder()
+            .target(latLng)
+            .zoom(16f)
+            .build()
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(position))
     }
 }
