8fea5888eddea7d1b49ace8933fd009b1697140f
wily.yang <ehdrnr1178@gmail.com>
2022-02-18 19:38:00 +0900

    page 640 complete

--- Chap19/ch19_map/src/main/java/com/example/ch19_map/MainActivity.kt
+++ Chap19/ch19_map/src/main/java/com/example/ch19_map/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.ch19_map
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
@@ -12,16 +14,39 @@ import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
 
-class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
-    GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+class MainActivity : AppCompatActivity(),OnMapReadyCallback,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+     {
 
+    private val permissionList = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_NETWORK_STATE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    var googleMap: GoogleMap? = null
     lateinit var providerClient: FusedLocationProviderClient
     lateinit var apiClient: GoogleApiClient
-    var googleMap: GoogleMap? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+        {
+            if (it.all { permission -> permission.value == true }) {
+                apiClientConnect()
+            } else {
+                Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,18 +54,19 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ){
-            if(it.all{permission -> permission.value == true}){
-                apiClient.connect()
-            } else {
-                Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
-            }
-        }
-
         (supportFragmentManager.findFragmentById(R.id.mapView) as
                 SupportMapFragment?)!!.getMapAsync(this)
+
+        if(permissionList.all { perm -> ContextCompat.checkSelfPermission(this, perm) == PackageManager.PERMISSION_GRANTED }){
+            apiClientConnect()
+        }else{
+            requestPermissionLauncher.launch(permissionList)
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap) { googleMap = map; moveMap(57.34534, 98.45646)}
+
+    private fun apiClientConnect(){
         providerClient = LocationServices.getFusedLocationProviderClient(this)
         apiClient = GoogleApiClient.Builder(this)
             .addApi(LocationServices.API)
@@ -48,70 +74,39 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
             .addOnConnectionFailedListener(this)
             .build()
 
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) !==
-            PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) !==
-            PackageManager.PERMISSION_GRANTED ||
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_NETWORK_STATE
-            ) !==
-            PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissionLauncher.launch(
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.ACCESS_NETWORK_STATE
-                )
-            )
-        }else{
-            apiClient.connect()
-        }
+        apiClient.connect()
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == 100 &&
-                grantResults[0] === PackageManager.PERMISSION_GRANTED &&
-                grantResults[1] === PackageManager.PERMISSION_GRANTED &&
-                grantResults[2] === PackageManager.PERMISSION_GRANTED){
-            apiClient.connect()
+    override fun onConnected(p0: Bundle?) {
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            providerClient.lastLocation.addOnSuccessListener(this@MainActivity) { location ->
+                location?.let {
+                    moveMap(location.latitude, location.longitude)
+                }
+            }
+            apiClient.disconnect()
         }
     }
 
     private fun moveMap(latitude: Double, longitude: Double){
+        val latLng = LatLng(latitude, longitude)
+        val position : CameraPosition = CameraPosition.Builder()
+            .target(latLng)
+            .zoom(16f)
+            .build()
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(position))
 
+        val markerOptions = MarkerOptions()
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        markerOptions.position(latLng)
+        markerOptions.title("MyLocation")
+
+        googleMap?.addMarker(markerOptions)
     }
 
-    override fun onConnected(p0: Bundle?) {
-        if(ContextCompat.checkSelfPermission(
-                this@MainActivity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) === PackageManager.PERMISSION_GRANTED){
-
-        }
-    }
-
-    override fun onConnectionSuspended(p0: Int) {
-        //
-    }
-
-    override fun onConnectionFailed(p0: ConnectionResult) {
-        //
-    }
-
-    override fun onMapReady(p0: GoogleMap) {
-        googleMap = p0
-    }
+    override fun onConnectionSuspended(p0: Int) = Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_SHORT).show()
+    override fun onConnectionFailed(p0: ConnectionResult) = Toast.makeText(this, "onConnectionFailed", Toast.LENGTH_SHORT).show()
 }
