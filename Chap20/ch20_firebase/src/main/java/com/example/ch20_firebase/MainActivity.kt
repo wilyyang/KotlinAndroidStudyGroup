8f291c4024adf3fef7599b69d3c461220803842b
wilyyang <ehdrnr1178@gmail.com>
2022-02-22 23:00:56 +0900

    page 646 complete

--- Chap20/ch20_firebase/src/main/java/com/example/ch20_firebase/MainActivity.kt
+++ Chap20/ch20_firebase/src/main/java/com/example/ch20_firebase/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.ch20_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+        auth.createUserWithEmailAndPassword("email", "password")
+            .addOnCompleteListener(this){
+                task->
+
+            }
     }
 }
