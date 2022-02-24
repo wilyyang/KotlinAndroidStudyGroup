977e26ed3370c845087210ad69d3e8f8e52634ae
wily <ehdrnr1178@gmail.com>
2022-02-24 19:49:24 +0900

    page 670 complete

--- Chap20/app/src/main/java/com/example/chap20/MainActivity.kt
+++ Chap20/app/src/main/java/com/example/chap20/MainActivity.kt
@@ -9,9 +9,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -80,5 +84,28 @@ class MainActivity : AppCompatActivity() {
             .build()
         val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
         googleForResult.launch(signInIntent)
+
+        // Chapter 21
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+//        val colRef: CollectionReference = db.collection("users")
+//
+//        val docRef: Task<DocumentReference> = colRef.add(
+//            mapOf("name" to "kkang", "email" to "a@a.com", "avg" to 10))
+//
+//        docRef.addOnSuccessListener { Log.d("wly9", "DocumentSnapshot added with ID: ${it.id}") }
+//        docRef.addOnFailureListener { Log.w("wly9", "Error adding document", it) }
+
+        db.collection("users").add(mapOf("name" to "wily", "email" to "b@b.com", "avg" to 31))
+            .addOnSuccessListener { Log.d("wly9", "DocumentSnapshot added with ID: ${it.id}") }
+            .addOnFailureListener { Log.w("wly9", "Error adding document", it)  }
+
+        class User(val name:String, val email:String, val avg: Int, @JvmField val isAdmin: Boolean, val isTop: Boolean)
+        val user = User("kim", "kim@a.com", 20, true, true)
+        db.collection("users").add(user)
+
+        val user2 = User("lee", "lee@a.com", 30, false, false)
+        db.collection("users").document("ID01").set(user2)
+
+        db.collection("users").document("ID01")
     }
 }
--- Chap20/ch20_firebase/src/main/java/com/example/ch20_firebase/AuthActivity.kt
+++ Chap20/ch20_firebase/src/main/java/com/example/ch20_firebase/AuthActivity.kt
@@ -13,58 +13,63 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.GoogleAuthProvider
 
+private const val MODE_LOGIN = "login"
+private const val MODE_LOGOUT = "logout"
+private const val MODE_SIGNIN = "signin"
 class AuthActivity : AppCompatActivity() {
     lateinit var binding: ActivityAuthBinding
 
-    private val googleForResult =
+    private val googleLoginForResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(result?.data)
                 try {
                     val account = task.getResult(ApiException::class.java)!!
+
                     val credential = GoogleAuthProvider.getCredential(account.idToken, null)
                     MyApplication.auth.signInWithCredential(credential)
                         .addOnCompleteListener(this) { task->
                             if(task.isSuccessful){
                                 MyApplication.email = account.email
-                                changeVisibility("login")
+                                changeVisibility(MODE_LOGIN)
                             }else{
-                                changeVisibility("logout")
+                                changeVisibility(MODE_LOGOUT)
                             }
                         }
                 }catch(e: ApiException){
-                    changeVisibility("logout")
+                    changeVisibility(MODE_LOGOUT)
                 }
             }
         }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         if(MyApplication.checkAuth()){
-            changeVisibility("login")
+            changeVisibility(MODE_LOGIN)
         }else{
-            changeVisibility("logout")
+            changeVisibility(MODE_LOGOUT)
         }
 
         binding.logoutBtn.setOnClickListener {
             MyApplication.auth.signOut()
             MyApplication.email = null
-            changeVisibility("logout")
+            changeVisibility(MODE_LOGOUT)
         }
 
         binding.goSignInBtn.setOnClickListener {
-            changeVisibility("signin")
+            changeVisibility(MODE_SIGNIN)
         }
 
         binding.googleLoginBtn.setOnClickListener {
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.app_name))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
             val signInIntent = GoogleSignIn.getClient(this, gso).signInIntent
-            googleForResult.launch(signInIntent)
+            googleLoginForResult.launch(signInIntent)
         }
 
         binding.signBtn.setOnClickListener {
@@ -80,24 +85,43 @@ class AuthActivity : AppCompatActivity() {
                             ?.addOnCompleteListener { sendTask->
                                 if(sendTask.isSuccessful){
                                     Toast.makeText(baseContext, "회원가입에 성공하였습니다. 전송된 메일을 확인해 주세요.", Toast.LENGTH_SHORT).show()
-
-                                    changeVisibility("logout")
+                                    changeVisibility(MODE_LOGOUT)
                                 }else{
                                     Toast.makeText(baseContext, "메일 전송 실패", Toast.LENGTH_SHORT).show()
-
-                                    changeVisibility("logout")
+                                    changeVisibility(MODE_LOGOUT)
                                 }
                             }
-                    }else{
+                    } else {
                         Toast.makeText(baseContext, "회원가입 실패", Toast.LENGTH_SHORT).show()
-                        changeVisibility("logout")
+                        changeVisibility(MODE_LOGOUT)
+                    }
+                }
+        }
+
+        binding.loginBtn.setOnClickListener {
+            val email: String = binding.authEmailEditView.text.toString()
+            val password: String = binding.authPasswordEditView.text.toString()
+
+            MyApplication.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.authEmailEditView.text.clear()
+                    binding.authPasswordEditView.text.clear()
+                    if(task.isSuccessful){
+                        if(MyApplication.checkAuth()){
+                            MyApplication.email = email
+                            changeVisibility(MODE_LOGIN)
+                        }else{
+                            Toast.makeText(baseContext, "전송된 메일로 이메일 인증이 되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }else{
+                        Toast.makeText(baseContext, "로그인 실패", Toast.LENGTH_SHORT).show()
                     }
                 }
         }
     }
 
     private fun changeVisibility(mode: String){
-        if(mode == "login"){
+        if(mode == MODE_LOGIN){
             binding.run {
                 authMainTextView.text = "${MyApplication.email} 님 반갑습니다."
                 logoutBtn.visibility = View.VISIBLE
@@ -108,7 +132,7 @@ class AuthActivity : AppCompatActivity() {
                 signBtn.visibility = View.GONE
                 loginBtn.visibility = View.GONE
             }
-        }else if(mode == "logout"){
+        }else if(mode == MODE_LOGOUT){
             binding.run {
                 authMainTextView.text = "로그인 하거나 회원가입 해주세요."
                 logoutBtn.visibility = View.GONE
@@ -119,7 +143,7 @@ class AuthActivity : AppCompatActivity() {
                 signBtn.visibility = View.GONE
                 loginBtn.visibility = View.VISIBLE
             }
-        }else if(mode == "signin"){
+        }else if(mode == MODE_SIGNIN){
             binding.run {
                 logoutBtn.visibility = View.GONE
                 goSignInBtn.visibility = View.GONE
